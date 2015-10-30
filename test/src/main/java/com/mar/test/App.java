8f224aac43cb23c2005644140287f3d0dacd71e2
Mariusz Kowalczyk <mariusz_kow@wp.pl>
2015-10-30 20:59:21 +0100

    Formatting and changed

--- test/src/main/java/com/mar/test/App.java
+++ test/src/main/java/com/mar/test/App.java
@@ -4,8 +4,9 @@ package com.mar.test;
  * Hello world!
  *
  */
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!11");
 	}
 }
