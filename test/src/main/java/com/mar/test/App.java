f52cc01a31b7c68feb88cf8207ee05c9c6da58ce
Mariusz Kowalczyk <mariusz_kow@wp.pl>
2016-08-12 18:16:45 +0200

    My changes for rel-1.2. Jeszcze jedna

--- test/src/main/java/com/mar/test/App.java
+++ test/src/main/java/com/mar/test/App.java
@@ -5,6 +5,7 @@ package com.mar.test;
  */
 public class App {
     // Moja zmiana na rel-1.2
+    // I jeszcze jedna zmiana
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
