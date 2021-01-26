4c8838ac2757ddec1fc31c8dc12c9e91b84bc6b6
Mariusz <mariusz_kow@wp.pl>
2021-01-26 17:55:50 +0100

    Update App

--- test/src/main/java/com/mar/test/App.java
+++ test/src/main/java/com/mar/test/App.java
@@ -8,7 +8,7 @@ public class App {
     // I jeszcze jedna zmiana
     // Remote change
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("World!");
     }
 // another change
 //yet another
